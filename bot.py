6a0549382922a20a968ea216d5a84bc5a18d0628
cyberjabka <76742792+sichiiii@users.noreply.github.com>
2023-05-12 12:27:11 +0300

    v1

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ def handle_message(update, context):
     try:
         context.bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING)
         context.bot.delete_message(message_id=message.message_id, chat_id=chat_id)
-        keyboard = [[InlineKeyboardButton("👍", callback_data="thumbs_up")]]
+        keyboard = [[InlineKeyboardButton("🔥", callback_data="fire")]]
         markup = InlineKeyboardMarkup(keyboard)
 
         if message.text:
@@ -76,36 +76,9 @@ def handle_reaction(update, context):
     query = update.callback_query
     chat_id = query.message.chat_id
     message_id = query.message.message_id
-    user_id = query.from_user.id
     reaction = query.data
-    if query.message.text:
-        text = query.message.text
-    elif query.message.caption:
-        text = query.message.caption
-    else:
-        text = ''
 
-    if reaction == "thumbs_up":
-        context.bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING)
-        context.bot.delete_message(message_id=message_id, chat_id=chat_id)
-        keyboard = [[InlineKeyboardButton("🔥", callback_data="fire")]]
-        markup = InlineKeyboardMarkup(keyboard)
-
-        if query.message.photo:
-            context.bot.send_photo(chat_id=chat_id, photo=query.message.photo[-1], caption=text, reply_markup=markup)
-        elif query.message.document:
-            document = query.message.document.file_id
-            context.bot.send_document(chat_id=chat_id, caption=text, document=document, reply_markup=markup)
-        elif query.message.video:
-            video = query.message.video.file_id
-            context.bot.send_video(chat_id=chat_id, caption=text, video=video, reply_markup=markup)
-        elif query.message.audio:
-            audio = query.message.audio.file_id
-            context.bot.send_audio(chat_id=chat_id, caption=text, audio=audio, reply_markup=markup)
-        else:
-            context.bot.send_message(chat_id=chat_id, text=text, reply_markup=markup)
-
-    elif reaction == "fire":
+    if reaction == "fire":
         context.bot.delete_message(chat_id=chat_id, message_id=message_id)
 
 
